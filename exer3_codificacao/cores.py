cd4696ec8178653474039e459dd3bf59dfa1afc8
gabrielearantesfatec-ops <gabriele.arantes.fatec@gmail.com>
2025-11-04 21:57:49 -0300

    Add files via upload

---
+++ exer3_codificacao/cores.py
@@ -0,0 +1,13 @@
+cor = input("Digite uma cor: ")
+
+if cor not in ("AZUL", "VERMELHO", "AMARELO"):
+  print("Essa cor não é primária!")
+  
+elif cor == "AZUL":
+  print("\nAzul: Calma, Inteligência, Frio")
+    
+elif cor == "VERMELHO":
+  print("\nVermelho: calor, energia, perigo")
+    
+elif cor == "AMARELO":
+  print("\nAmarelo: alegria, riqueza, luz")
